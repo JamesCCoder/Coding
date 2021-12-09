3c78af5642330d5bdf978ac4434bfad6b265afd2
James W <95759159+JamesCCoder@users.noreply.github.com>
2021-12-09 14:06:29 -0600

    Create fibonacci-number.js

---
+++ 509-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,9 @@
+var fib = (n) => {
+    let result = [];
+    result[0] = 0;
+    result[1] = 1;
+    for(let i = 2; i <= n; i++){
+          result[i] = result[i-1]+result[i-2];
+    }
+    return result[n];
+};
