aa5b85a63c556a38e0d678993f6f643558062d5e
James W <95759159+JamesCCoder@users.noreply.github.com>
2021-12-18 23:49:04 -0600

    Time: 0 ms (100.00%), Space: 38.3 MB (99.62%) - LeetHub

---
+++ 1064-fixed-point/1064-fixed-point.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fixedPoint(int[] arr) {
+        int res = -1;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == i){
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+}
