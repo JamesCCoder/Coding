86a125d560da5de8f7fc00ae401806391be79d78
James W <95759159+JamesCCoder@users.noreply.github.com>
2021-12-20 12:15:45 -0600

    Time: 1 ms (98.94%), Space: 37 MB (81.88%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(Character ch: s.toCharArray()){
+            if(ch == '('){
+                stack.push(')');
+            }else if(ch == '['){
+                stack.push(']');
+            }else if(ch == '{'){
+                stack.push('}');
+            }else{
+                if(stack.isEmpty() || stack.pop() != ch){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
