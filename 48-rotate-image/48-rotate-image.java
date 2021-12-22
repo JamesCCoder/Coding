441c34ba3bd463e6b4ae4e45a1b67cf9b1808ed1
James W <95759159+JamesCCoder@users.noreply.github.com>
2021-12-21 22:02:30 -0600

    Time: 0 ms (100.00%), Space: 38.9 MB (91.14%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -7,7 +7,7 @@ class Solution {
                 matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
                 matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
                 matrix[j][n-i-1] = matrix[i][j];
-                matrix[i][j]=temp;
+                matrix[i][j] = temp;
             }
         }
     }
