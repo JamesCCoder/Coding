562a48c478efc36e20ac5d0758e272e9edd01e4e
James W <95759159+JamesCCoder@users.noreply.github.com>
2021-12-22 10:30:16 -0600

    Time: 1 ms (99.88%), Space: 41.4 MB (85.18%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -10,37 +10,34 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        // find the middle of linked list [Problem 876]
-    // in 1->2->3->4->5->6 find 4 
-    ListNode slow = head, fast = head;
-    while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-
-    // reverse the second part of the list [Problem 206]
-    // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
-    // reverse the second half in-place
-    ListNode prev = null, curr = slow, tmp;
-    while (curr != null) {
-      tmp = curr.next;
-
-      curr.next = prev;
-      prev = curr;
-      curr = tmp;
-    }
-
-    // merge two sorted linked lists [Problem 21]
-    // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
-    ListNode first = head, second = prev;
-    while (second.next != null) {
-      tmp = first.next;
-      first.next = second;
-      first = tmp;
-
-      tmp = second.next;
-      second.next = first;
-      second = tmp;
-    }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        ListNode pre = null;
+        ListNode cur = slow;
+        ListNode temp;
+        while(cur != null){
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        
+        ListNode first = head;
+        ListNode second = pre;
+        while(second.next != null){
+            temp = first.next;
+            first.next = second;
+            first = temp;
+            
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
+            
     }
 }
