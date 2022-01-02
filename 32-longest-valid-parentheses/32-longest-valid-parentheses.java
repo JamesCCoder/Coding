6c5b8c62b960c288d1b9fd72f89db84204408e96
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-01 23:22:32 -0600

    Time: 4 ms (21.67%), Space: 39.6 MB (21.00%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                left++;
+            }else{
+                right++;
+            }
+            
+            if(left == right){
+                maxLength = Math.max(maxLength, right * 2);
+            }else if(right >= left){
+                left = 0;
+                right = 0;
+            }
+        }
+        
+        left = 0;
+        right = 0;
+        
+        for(int i = s.length()-1; i>=0; i--){
+            if(s.charAt(i) == ')'){
+                right++;
+            }else{
+                left++;
+            }
+            
+            if(right == left){
+                maxLength = Math.max(maxLength, left*2);
+            }else if(left >= right){
+                left = 0;
+                right = 0;
+            }
+        }
+        return maxLength;
+    }
+}
