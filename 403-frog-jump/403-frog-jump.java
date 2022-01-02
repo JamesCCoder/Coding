5f790ead04e10e37aec4803318c4d8630c007baf
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-01 19:45:45 -0600

    Time: 45 ms (48.75%), Space: 70.5 MB (15.31%) - LeetHub

--- 403-frog-jump/403-frog-jump.java
+++ 403-frog-jump/403-frog-jump.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        int n =  stones.length;
+        int n = stones.length;
         boolean[][] dp = new boolean[n][n];
         dp[0][0] = true;
         
@@ -12,7 +12,7 @@ class Solution {
             for(int j = i-1; j>=0; j--){
                 int k = stones[i] - stones[j];
                 if(k > j+1) break;
-                dp[i][k] = dp[j][k-1]||dp[j][k]||dp[j][k+1];
+                dp[i][k] = dp[j][k-1] || dp[j][k] || dp[j][k+1];
                 if(i == n-1 && dp[i][k]) return true;
             }
         }
