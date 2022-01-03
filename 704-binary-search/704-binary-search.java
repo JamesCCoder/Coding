a1af57e8b56988658a9e15957cf27410cbc0bdae
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-02 20:51:46 -0600

    Time: 0 ms (100.00%), Space: 52 MB (5.78%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while(l <= r){
+            int m = (r - l)/2 + l;
+            if(nums[m] == target) return m;
+            if(target < nums[m]){
+                r = m -1;
+            }else{
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+}
