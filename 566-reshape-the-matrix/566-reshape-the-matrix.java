c7957a8350356eda5998799c5ff216fdceb95584
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-06 10:31:30 -0600

    Time: 1 ms (45.21%), Space: 47.9 MB (15.06%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -2,16 +2,16 @@ class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         int m = mat.length;
         int n = mat[0].length;
-        if(m*n != r*c) {
-            return mat;
-        }
-        int res[][] = new int[r][c];
+        
+        if(m * n != r *c) return mat;
+        
+        int[][] res = new int[r][c];
         int x = 0;
         int y = 0;
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                res[x][y] = mat[i][j];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+               res[x][y] = mat[i][j];
                 y++;
                 
                 if(y == c) {
