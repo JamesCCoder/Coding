c3601fa319f1fb2f6b60b029865aacd016aaeabe
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-06 16:06:58 -0600

    Time: 192 ms (8.11%), Space: 168.5 MB (5.99%) - LeetHub

---
+++ 1836-remove-duplicates-from-an-unsorted-linked-list/1836-remove-duplicates-from-an-unsorted-linked-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicatesUnsorted(ListNode head) {
+        HashMap<Integer, Integer> r = new HashMap<>();
+        for(ListNode i = head; i != null; i = i.next){
+            r.put(i.val, r.getOrDefault(i.val, 0) + 1);
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        
+        while(cur.next != null){
+            if(r.get(cur.next.val) > 1){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
