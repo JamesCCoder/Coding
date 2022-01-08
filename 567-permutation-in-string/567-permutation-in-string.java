3c58b5c25422b4ea78b39c99bf5466bb57783ddd
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-08 09:44:02 -0600

    Update 567-permutation-in-string.java

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -3,26 +3,20 @@ class Solution {
         int len1 = s1.length(), len2 = s2.length();
         if (len1 > len2) return false;
         
-        int[] count = new int[26];
+        int[] count1 = new int[128];
+        int[] count2 = new int[128];
         for (int i = 0; i < len1; i++) {
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
+            count1[s1.charAt(i)]++;
+            count2[s2.charAt(i)]++;
         }
-        if (allZero(count)) return true;
+        if (Arrays.equals(count1, count2)) return true;
         
         for (int i = len1; i < len2; i++) {
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - len1) - 'a']++;
-            if (allZero(count)) return true;
+            count2[s2.charAt(i)]++;
+            count2[s2.charAt(i - len1)]--;
+            if (Arrays.equals(count1, count2)) return true;
         }
         
         return false;
-    }
-    
-    private boolean allZero(int[] count) {
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
-        }
-        return true;
     }
 }
