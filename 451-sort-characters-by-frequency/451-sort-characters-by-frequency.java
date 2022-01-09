4463c3370c8f43d9a1f8f2fa6ada255322830c4f
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-08 22:43:38 -0600

    Time: 29 ms (28.26%), Space: 46.8 MB (16.40%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String frequencySort(String s) {
+        StringBuilder res = new StringBuilder();
+        if (s == null || s.length() == 0) {
+            return res.toString();
+        }
+        // A map of each char and its frequency
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+        
+        // An array of lists of chars, at the index (frequency)
+        List<Character> [] bucket = new List[s.length() + 1]; 
+        // An short way to iterate map
+        for (char key : map.keySet()) {
+            // build a frequency list of each char
+            int freq = map.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<Character>();
+            }
+            bucket[freq].add(key);
+        }
+        // Iterate the frequency list from the end to start (index high to low)
+        // build the result string
+        for (int i = bucket.length-1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (char c : bucket[i]) {
+                    // append 'frequnecy' times
+                    for (int j = 0; j < i; j++) {
+                        res.append(c);    
+                    }
+                }     
+            }
+        }
+        return res.toString();
+        
+    }
+}
