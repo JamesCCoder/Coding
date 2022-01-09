af95b689b3f21dfafc4e5af620b807833c3e143b
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-09 00:40:23 -0600

    Time: 1 ms (58.45%), Space: 44.9 MB (21.91%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -7,14 +7,13 @@ class Solution {
         return image;
     }
     
-    public void helper(int[][] image, int x, int y, int m, int n, int orgColor, int newColor){
-        if( x<0 || x>= m || y<0 || y>=n) return;
-        if(image[x][y] != orgColor) return;
-        
+    public void helper(int[][] image, int x, int y, int m, int n, int oldColor, int newColor){
+        if(x < 0 || x>=m || y<0 || y>=n) return;
+        if(image[x][y] != oldColor) return;
         image[x][y] = newColor;
-        helper(image, x+1, y, m, n, orgColor, newColor);
-        helper(image, x-1, y, m, n, orgColor, newColor);
-        helper(image, x, y+1, m, n, orgColor, newColor);
-        helper(image, x, y-1, m, n, orgColor, newColor);
+        helper(image, x+1, y, m, n, oldColor, newColor);
+        helper(image, x-1, y, m, n, oldColor, newColor);
+        helper(image, x, y+1, m, n, oldColor, newColor);
+        helper(image, x, y-1, m, n, oldColor, newColor);
     }
 }
