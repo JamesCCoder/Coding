d143e57377bdad40d23217307c13e224c02f24a2
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-09 01:19:15 -0600

    Time: 3 ms (67.75%), Space: 55.6 MB (20.00%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0){
-            return 0;
-        }
-        int res = 0;
+        if(grid.length == 0 || grid == null) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
         
-        for(int i = 0; i< grid.length; i++){
-            for(int j = 0; j<grid[0].length; j++){
+        int res = 0;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 if(grid[i][j] == '1'){
                     res++;
                     helper(grid, i, j);
@@ -16,15 +16,12 @@ class Solution {
         return res;
     }
     
-    public void helper(char[][] grid, int i, int j){
-        if(i<0 || j<0 || i>= grid.length || j >= grid[0].length || grid[i][j] != '1'){
-            return;
-        }
-        
-        grid[i][j] = '*';
-        helper(grid, i, j+1);
-        helper(grid, i, j-1);
-        helper(grid, i+1, j);
-        helper(grid, i-1, j);
+    public void helper(char[][] grid, int x, int y){
+        if(x<0 || x>= grid.length || y<0 || y>= grid[0].length || grid[x][y] != '1') return;
+        grid[x][y] = '*';
+        helper(grid, x+1, y);
+        helper(grid, x-1, y);
+        helper(grid, x, y+1);
+        helper(grid, x, y-1);
     }
 }
