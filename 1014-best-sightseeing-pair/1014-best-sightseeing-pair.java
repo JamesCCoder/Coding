58940efeb11f821732ec3b15de5a7a6a8d8ac101
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-09 12:08:23 -0600

    Update 1014-best-sightseeing-pair.java

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,17 +1,12 @@
 class Solution {
 
     
-        public int maxScoreSightseeingPair(int[] values) {
-        int n=values.length;
-        int max=Integer.MIN_VALUE;
-        int temp=values[0];
-        for(int i=1;i<n;i++){
-            int t=values[i]+i;
-            max=Math.max(max,temp+values[i]-i);
-            temp=Math.max(temp,t);
+public int maxScoreSightseeingPair(int[] values) {
+        int res = 0, n = values.length, mx = 0;
+        for (int i = 0; i < n; ++i) {
+            res = Math.max(res, mx + values[i] - i);
+            mx = Math.max(mx, values[i] + i);
         }
-        return max;
-   
-            
+        return res;
     }
 }
