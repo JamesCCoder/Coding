0b420d438397f8878942977e8ffee88e3f5a7ef7
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-12 13:21:00 -0600

    Time: 0 ms (100.00%), Space: 38.7 MB (11.07%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,23 +1,16 @@
 class MyQueue {
 
-   private Stack<Integer> in = null;
-    private Stack<Integer> out = null;
-    /**
-     * Initialize your data structure here.
-     */
+   private Stack<Integer> in = new Stack<>();
+    private Stack<Integer> out = new Stack<>();
+
     public MyQueue() {
-        in = new Stack<>();
-        out = new Stack<>();
+      
     }
-    /**
-     * Push element x to the back of queue.
-     */
+
     public void push(int x) {
         in.push(x);
     }
-    /**
-     * Removes the element from in front of queue and returns that element.
-     */
+
     public int pop() {
         if (out.empty()) {
             int size = in.size();
@@ -26,9 +19,7 @@ class MyQueue {
         }
         return out.pop();
     }
-    /**
-     * Get the front element.
-     */
+  
     public int peek() {
         if (out.empty()) {
             int size = in.size();
@@ -37,14 +28,14 @@ class MyQueue {
         }
         return out.peek();
     }
-    /**
-     * Returns whether the queue is empty.
-     */
+
     public boolean empty() {
         return in.empty() && out.empty();
     }
 
 }
+
+
 
 /**
  * Your MyQueue object will be instantiated and called as such:
