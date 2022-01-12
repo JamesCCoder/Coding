f27e0e612b7dc1746c8f7ad5ca000caae93dcb0a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-12 14:00:41 -0600

    Time: 0 ms (100.00%), Space: 37 MB (49.53%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,41 +1,40 @@
 class MyQueue {
 
-   private Stack<Integer> in = new Stack<>();
-    private Stack<Integer> out = new Stack<>();
-
+    Stack<Integer> in = new Stack<>();
+    Stack<Integer> out = new Stack<>();
+    
     public MyQueue() {
-      
+        
     }
-
+    
     public void push(int x) {
         in.push(x);
     }
-
+    
     public int pop() {
-        if (out.empty()) {
-            int size = in.size();
-            for (int i = 0; i < size; i++)
+        if(out.isEmpty()){
+            int s = in.size();
+            for(int i = 0; i<s; i++){
                 out.push(in.pop());
+            }
         }
         return out.pop();
     }
-  
+    
     public int peek() {
-        if (out.empty()) {
-            int size = in.size();
-            for (int i = 0; i < size; i++)
+        if(out.isEmpty()){
+            int s = in.size();
+            for(int i = 0; i<s; i++){
                 out.push(in.pop());
+            }
         }
         return out.peek();
     }
-
+    
     public boolean empty() {
-        return in.empty() && out.empty();
+        return in.isEmpty() && out.isEmpty();
     }
-
 }
-
-
 
 /**
  * Your MyQueue object will be instantiated and called as such:
