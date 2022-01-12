a0bc1f8090fb8a91b09709da2eacf762861185ff
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-11 21:57:29 -0600

    Time: 1 ms (14.72%), Space: 38.9 MB (20.74%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int res = 0;
+        int cur = 0;
+        for(int i = 2; i<nums.length; i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                cur++;
+                res += cur;
+            }else{
+                cur = 0;
+            } 
+        }
+        return res;
+    }
+}
