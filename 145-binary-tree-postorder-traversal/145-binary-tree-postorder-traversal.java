c65b07cb91e8715d8c686f3f24440f299ae109d1
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-11 21:10:17 -0600

    Update 145-binary-tree-postorder-traversal.java

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -27,3 +27,17 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res= new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+            res.addAll(postorderTraversal(root.left));
+            res.addAll(postorderTraversal(root.right));
+            res.add(root.val);
+            return res; 
+        
+    }
+}
