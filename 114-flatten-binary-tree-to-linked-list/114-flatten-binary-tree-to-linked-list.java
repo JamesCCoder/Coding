b9dd61b1d61d201f723fbda4cef6aaa4a407ef65
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-13 17:31:31 -0600

    Update 114-flatten-binary-tree-to-linked-list.java

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,35 +14,21 @@
  * }
  */
 class Solution {
-   
     public void flatten(TreeNode root) {
-        
-        // Handle the null scenario
-        if (root == null) {
-            return;
-        }
-        
-        TreeNode node = root;
-        
-        while (node != null) {
-            
-            // If the node has a left child
-            if (node.left != null) {
-                
-                // Find the rightmost node
-                TreeNode rightmost = node.left;
-                while (rightmost.right != null) {
-                    rightmost = rightmost.right;
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode next = curr.left;
+                TreeNode predecessor = next;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
                 }
-                
-                // rewire the connections
-                rightmost.right = node.right;
-                node.right = node.left;
-                node.left = null;
+                predecessor.right = curr.right;
+                curr.left = null;
+                curr.right = next;
             }
-            
-            // move on to the right side of the tree
-            node = node.right;
+            curr = curr.right;
         }
     }
 }
+
