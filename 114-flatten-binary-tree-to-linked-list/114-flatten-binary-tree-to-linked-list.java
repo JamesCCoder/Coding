298571139c2d11c4fac78c00b1df67d9b3147b5a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-14 10:05:10 -0600

    Update 114-flatten-binary-tree-to-linked-list.java

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -15,20 +15,28 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        TreeNode curr = root;
-        while (curr != null) {
-            if (curr.left != null) {
-                TreeNode next = curr.left;
-                TreeNode predecessor = next;
-                while (predecessor.right != null) {
-                    predecessor = predecessor.right;
-                }
-                predecessor.right = curr.right;
-                curr.left = null;
-                curr.right = next;
-            }
-            curr = curr.right;
+    while (root != null) { 
+        //左子树为 null，直接考虑下一个节点
+        if (root.left == null) {
+            root = root.right;
+        } else {
+            // 找左子树最右边的节点
+            TreeNode pre = root.left;
+            while (pre.right != null) {
+                pre = pre.right;
+            } 
+            //将原来的右子树接到左子树的最右边节点
+            pre.right = root.right;
+            // 将左子树插入到右子树的地方
+            root.right = root.left;
+            root.left = null;
+            // 考虑下一个节点
+            root = root.right;
         }
     }
 }
+
+
+}
+
 
