fbff1bc6c75b27d18f79536350a51e7e061ea022
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-15 09:39:59 -0600

    Time: 6 ms (38.09%), Space: 44 MB (40.89%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -3,18 +3,21 @@ class Solution {
         if(matrix.length == 1) return matrix[0][0];
         int n = matrix.length;
         int res = Integer.MAX_VALUE;
-        
-        for(int i = 1; i<n; i++){
-            for(int j =0; j<n; j++){
-                int pre = matrix[i - 1][j];
-                if (j > 0) pre = Math.min(pre, matrix[i - 1][j - 1]);
-                if (j < n - 1) pre = Math.min(pre, matrix[i - 1][j + 1]);
+        for(int i = 1; i<n;i++){
+            for(int j = 0; j<n; j++){
+                int pre = matrix[i-1][j];
+                if(j > 0){
+                    pre = Math.min(pre, matrix[i-1][j-1]);
+                }
+                if(j < n - 1){
+                    pre = Math.min(pre, matrix[i-1][j+1]);
+                }
                 matrix[i][j] += pre;
-                if (i == n - 1) res = Math.min(res, matrix[i][j]);
+                if(i == n - 1){
+                    res = Math.min(res, matrix[i][j]);
+                }
             }
         }
         return res;
     }
 }
-
-
