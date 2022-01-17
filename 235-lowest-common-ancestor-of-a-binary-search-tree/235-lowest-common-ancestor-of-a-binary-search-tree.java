e108ef0deecfb2f99559624fe4dd55b9e7b113d2
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-17 09:32:20 -0600

    Update 235-lowest-common-ancestor-of-a-binary-search-tree.java

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -18,3 +18,15 @@ class Solution {
         return root;
     }
 }
+
+//https://blog.csdn.net/qq_21201267/article/details/100869188
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) return null;
+        if(left != null && right != null) return root;
+        return (left != null) ? left : right;
+    }
+}
