46a9ddb5fec1620e379bd6954b3bd2755af1969d
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-16 22:49:07 -0600

    Time: 17 ms (96.92%), Space: 41.4 MB (65.97%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,23 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        //细节，这里需要预存前缀和为 0 的情况，会漏掉前几位就满足的情况
-        //例如输入[1,1,0]，k = 2 如果没有这行代码，则会返回0,漏掉了1+1=2，和1+1+0=2的情况
-        //输入：[3,1,1,0] k = 2时则不会漏掉
-        //因为presum[3] - presum[0]表示前面 3 位的和，所以需要map.put(0,1),垫下底
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int res = 0;
+        
         map.put(0, 1);
-        int count = 0;
-        int presum = 0;
-        for (int x : nums) {
-            presum += x;
-            //当前前缀和已知，判断是否含有 presum - k的前缀和，那么我们就知道某一区间的和为 k 了。
-            if (map.containsKey(presum - k)) {
-                count += map.get(presum - k);//获取次数
+        for(int num : nums){
+            sum += num;
+            if(map.containsKey(sum - k)){
+                res += map.get(sum - k);
             }
-            //更新
-            map.put(presum,map.getOrDefault(presum,0) + 1);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        return count;
-
+        return res;
     }
 }
