1058f6b93f2d28d2111f86e39673f003e5e608be
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-17 22:53:56 -0600

    Time: 3 ms (35.63%), Space: 43.5 MB (7.12%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
@@ -15,37 +15,33 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        if(n < 1)
+        if(n < 1){
             return new ArrayList<>();
+        }
         return helper(1, n);
     }
-
+    
     public List<TreeNode> helper(int start, int end){
-        List<TreeNode> list = new ArrayList<>();
-
+        List<TreeNode> res = new ArrayList<>();
         if(start > end){
-            // 如果当前子树为空，不加null行吗？
-            list.add(null);
-            return list;
+            res.add(null);
+            return res;
         }
-
-        for(int i = start; i <= end; i++){
-            // 想想为什么这行不能放在这里，而放在下面？
-            // TreeNode root = new TreeNode(i);
-            List<TreeNode> left = helper(start, i-1);  
-            List<TreeNode> right = helper(i+1, end); 
-
-            // 固定左孩子，遍历右孩子
+        
+        for(int i = start; i<= end; i++){
+            List<TreeNode> left = helper(start, i - 1);
+            List<TreeNode> right = helper(i + 1, end);
+            
             for(TreeNode l : left){
                 for(TreeNode r : right){
                     TreeNode root = new TreeNode(i);
                     root.left = l;
                     root.right = r;
-                    list.add(root);
+                    res.add(root);
                 }
             }
+            
         }
-        return list;
-
+        return res;
     }
 }
