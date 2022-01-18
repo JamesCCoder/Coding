2b1b44d49bab7873504b712fb5c1e5624f05f73e
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-17 22:10:25 -0600

    Update 605-can-place-flowers.java

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        for (int i = 0, len = flowerbed.length; i < len && n > 0;) {
+        int i = 0;
+        while (i < flowerbed.length && n > 0) {
 		if (flowerbed[i] == 1) {
 			i += 2;
 		} else if (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) {
