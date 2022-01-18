b1eeeaac52291ea48d31430dc4113b89a9f0bfbb
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-18 13:28:07 -0600

    Update 179-largest-number.java

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -8,10 +8,8 @@ class Solution {
   
     Arrays.sort(arr, (s1,s2)-> (s2+s1).compareTo(s1+s2));
 
-    if("0".equals(arr[0])) return "0";
-
     StringBuilder sb = new StringBuilder();
     for(String s : arr) sb.append(s);
-    return sb.toString();
+    return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
