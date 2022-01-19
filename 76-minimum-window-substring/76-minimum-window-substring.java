d98464d5efe9a8522ce8fd37ef5c5aa9828b97ce
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-18 22:31:49 -0600

    Time: 12 ms (64.66%), Space: 40.6 MB (46.27%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,44 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> tfreq = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i<t.length(); i++){
-            tfreq.put(t.charAt(i), tfreq.getOrDefault(t.charAt(i), 0 ) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0 ) + 1);
         }
         
         HashMap<Character, Integer> window = new HashMap<>();
-        int sum = 0;
-        
         int start = 0;
         int len = Integer.MAX_VALUE;
         
         int left = 0;
         int right = 0;
         
+        int sum = 0;
+        
         while(right < s.length()){
-            char c = s.charAt(right);
-            if(tfreq.containsKey(c)){
-                window.put(c, window.getOrDefault(c, 0) + 1);
-                if(tfreq.get(c).equals(window.get(c))){
+            if(map.containsKey(s.charAt(right))){
+                window.put(s.charAt(right), window.getOrDefault(s.charAt(right), 0) + 1);
+                if(map.get(s.charAt(right)).equals(window.get(s.charAt(right)))){
                     sum++;
                 }
             }
             right++;
             
-            while(sum == tfreq.size()){
+            while(sum == map.size()){
                 if(right - left < len){
                     start = left;
                     len = right - left;
                 }
-                
-                char x = s.charAt(left);
-                if(tfreq.containsKey(x)){
-                    if(window.get(x).equals(tfreq.get(x))){
-                        sum--;
-                    }
-                    window.put(x, window.get(x) - 1);
+            
+            
+            
+            if(map.containsKey(s.charAt(left))){
+                if(window.get(s.charAt(left)).equals(map.get(s.charAt(left)))){
+                    
+                    sum--;
                 }
-                left++;
+                window.put(s.charAt(left), window.get(s.charAt(left)) - 1);//window.put(s.charAt(left), window.getOrDefault(s.charAt(left), 0) - 1);
             }
+            left++;
+            }   
         }
         return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
     }
