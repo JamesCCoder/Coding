3e2808bf4077f859f335413946f2df61a522d234
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-20 12:09:14 -0600

    Time: 6 ms (46.98%), Space: 39.4 MB (30.71%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,9 +1,8 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int[] set = new int[26];
-        Arrays.fill(set, -1);
+        int[] set = new int[128];
         for(int i = 0; i<s.length(); i++){
-            set[s.charAt(i) - 'a'] = i;
+            set[s.charAt(i)] = i;
         }
         
         int start = 0;
@@ -11,11 +10,11 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         
         for(int i = 0; i<s.length(); i++){
-            int cur = set[s.charAt(i) - 'a'];
+            int cur = set[s.charAt(i)];
             scan = Math.max(scan, cur);
             if(i == scan){
                 res.add(i - start + 1);
-                start = i + 1;
+                start  = i + 1;
             }
         }
         return res;
