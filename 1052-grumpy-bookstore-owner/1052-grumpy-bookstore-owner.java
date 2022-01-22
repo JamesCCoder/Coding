9705be0ff0964ad25d16d75240accfbe1247293e
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-22 10:04:15 -0600

    Time: 3 ms (72.60%), Space: 51.9 MB (29.62%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int sum = 0;
+        for(int i = 0; i<customers.length; i++){
+            if(grumpy[i] == 0){
+                sum += customers[i];
+                customers[i] = 0;
+            }
+        }
+        
+        int max = 0;
+        int cur = 0;
+        
+        for(int l = 0, r = 0; r<customers.length; r++){
+            cur += customers[r];
+            if(r - l + 1 > minutes){
+                cur -= customers[l++];
+            }
+            max = Math.max(max, cur);
+        }
+        return sum + max;
+    }
+}
