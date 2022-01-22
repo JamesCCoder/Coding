ffc5115839576a4d09518b1d259cd3e14356924a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-21 22:37:19 -0600

    Update 1510-stone-game-iv.java

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -10,3 +10,19 @@ class Solution {
         return dp[n];
     }
 }
+
+
+class Solution {
+    public boolean winnerSquareGame(int n) {
+      boolean dp[] = new boolean[n+1];
+      for(int i=0;i<=n;i++){
+        if(dp[i]){
+            continue;
+        } 
+        for(int k=1;i+k*k<=n;k++){
+            dp[i+k*k]=true;
+        }
+      }      
+    return dp[n];
+}
+}
