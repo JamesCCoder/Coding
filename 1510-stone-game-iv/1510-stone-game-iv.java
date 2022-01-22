74f006f9e4c246a804225d9e7063baabfd17f423
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-21 22:02:52 -0600

    Time: 44 ms (53.18%), Space: 38.3 MB (58.38%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = !dp[i - j * j];
+                if (dp[i]) break;
+            }
+        }
+        return dp[n];
+    }
+}
