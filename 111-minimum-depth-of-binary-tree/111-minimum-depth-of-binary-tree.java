a8100b7d80d739bfe494d8e886972ddb012e1a9e
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-24 09:25:18 -0600

    Time: 15 ms (21.20%), Space: 93.3 MB (21.73%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -14,16 +14,16 @@
  * }
  */
 class Solution {
-public int minDepth(TreeNode root) {
-    if(root == null) {
-        return 0;
-    } else if(root.left == null && root.right == null) {
-        return 1;
-    } else if(root.left == null) {
-        return minDepth(root.right) + 1;
-    } else if(root.right == null) {
-        return minDepth(root.left) + 1;
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }else if(root.left == null && root.right == null){
+            return 1;
+        }else if(root.left == null){
+            return minDepth(root.right) + 1;
+        }else if(root.right == null){
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left) + 1, minDepth(root.right) + 1);
     }
-    return Math.min(minDepth(root.left) + 1, minDepth(root.right) + 1);
-}
 }
