1ae9e566967603ed280df9124382926f6ed23f65
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-24 21:36:16 -0600

    Time: 2 ms (56.28%), Space: 51.8 MB (13.36%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length <= 2) return false;
+        
+        int i = 0;
+        
+        while(i < arr.length - 1 && arr[i]<arr[i+1]){
+            i++;
+        }
+        
+        if(i == 0 || i == arr.length - 1){
+            return false;
+        }
+        
+        while(i < arr.length - 1 && arr[i] > arr[i+1]){
+            i++;
+        }
+        
+        return i == arr.length - 1;
+    }
+}
