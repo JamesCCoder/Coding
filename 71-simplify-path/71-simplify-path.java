1e13b1f95aeacc32f4c16901086f9a5e6f632c8c
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-01-28 09:35:53 -0600

    Time: 6 ms (60.12%), Space: 43.6 MB (5.87%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String simplifyPath(String path) {
+         Stack<String> stack = new Stack<>();
+        String[] split = path.split("/");
+        for (String s : split) {
+            if (s.equals("") || s.equals(".")) continue;
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(s);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (String s : stack) {
+            res.append("/");
+            res.append(s);
+        }
+        if (res.length() == 0) return "/";
+        return res.toString();
+    }
+}
