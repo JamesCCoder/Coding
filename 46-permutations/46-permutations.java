53af9e971b0a1e2a4ae8bd8675fdc7a18fb57a91
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-10 14:32:17 -0600

    Time: 2 ms (71.39%), Space: 44.9 MB (8.40%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        
         List<List<Integer>> res = new ArrayList<>();
-        if(nums.length < 1){
-            return res;
-        }
-        helper(res, new ArrayList<>(), nums, new boolean[nums.length]);    
+        if(nums.length<1) return res;
+        helper(res, new ArrayList<>(), nums, new boolean[nums.length]);
         return res;
     }
     
     public void helper(List<List<Integer>> res, List<Integer> cur, int[] nums, boolean[] visited){
         if(cur.size() == nums.length){
             res.add(new ArrayList(cur));
-            return;
         }
         
         for(int i = 0; i<nums.length; i++){
@@ -19,7 +17,7 @@ class Solution {
             cur.add(nums[i]);
             visited[i] = true;
             helper(res, cur, nums, visited);
-            cur.remove(cur.size() - 1);
+            cur.remove(cur.size()-1);
             visited[i] = false;
         }
     }
