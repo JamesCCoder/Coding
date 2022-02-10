949e5568745cdb28b38cc336a3da92a2c556839e
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-09 20:32:45 -0600

    Time: 20 ms (9.95%), Space: 50.5 MB (5.14%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,37 +1,37 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> map =new  HashMap<>();
-        map.put("M",1000);
-        map.put("CM",900);
-        map.put("D",500);
-        map.put("CD",400);
-        map.put("C",100);
-        map.put("XC",90);
-        map.put("L",50);
-        map.put("XL",40);
-        map.put("X",10);
-        map.put("IX",9);
-        map.put("V",5);
-        map.put("IV",4);
-        map.put("I",1);
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("M", 1000);
+        map.put("CM", 900);
+        map.put("D", 500);
+        map.put("CD", 400);
+        map.put("C", 100);
+        map.put("XC", 90);
+        map.put("L", 50);
+        map.put("XL", 40);
+        map.put("X", 10);
+        map.put("IX", 9);
+        map.put("V", 5);
+        map.put("IV", 4);
+        map.put("I", 1);
         
+        int index = 0;
         int res = 0;
-        int i = 0;
         
-        while(i<s.length()){
-            if(i<s.length()-1){
-                String db = s.substring(i,i+2);
+        while(index < s.length()){
+            if(index < s.length()-1){
+                String db = s.substring(index, index + 2);
                 if(map.containsKey(db)){
                     res += map.get(db);
-                    i += 2;
+                    index = index + 2;
                     continue;
                 }
             }
-            String sg = s.substring(i, i+1);
+            String sg = s.substring(index, index + 1);
             if(map.containsKey(sg)){
-                    res += map.get(sg);
-                    i += 1;
-                }
+                res += map.get(sg);
+                index = index + 1;
+            }
         }
         return res;
     }
