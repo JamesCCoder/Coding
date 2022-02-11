8fa84048e4c6a3bd0d265d3f5427dc64922a44df
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-11 14:44:09 -0600

    Time: 1 ms (44.25%), Space: 41 MB (11.86%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,7 +4,7 @@ class Solution {
         res[0][0] = 1;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(i>0 && j >0){
+                if(i > 0 && j > 0){
                     res[i][j] = res[i-1][j] + res[i][j-1];
                 }else if(i > 0){
                     res[i][j] = res[i-1][j];
