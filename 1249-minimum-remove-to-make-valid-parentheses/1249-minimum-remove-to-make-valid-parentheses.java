ab530c15e6927f0666a7acc346caa525109b299a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-12 11:18:29 -0600

    Time: 16 ms (84.06%), Space: 49.1 MB (47.51%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,33 +1,25 @@
 class Solution {
-    /**
-     * 两次遍历
-     *  第一次遍历: 去除多余的右括号
-     *  第二次遍历: 去除多余的左括号
-     */
     public String minRemoveToMakeValid(String s) {
-        int length = s.length();
         StringBuilder sb = new StringBuilder();
-        //去除多余的右括号
-        int leftCount = 0;
-        for (int i = 0; i < length; i++) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '('){
-                leftCount++;
+                count++;
                 sb.append(c);
             } else if (c == ')'){
-                if (leftCount > 0){
-                    leftCount--;
+                if (count > 0){
+                    count--;
                     sb.append(c);
                 }
             } else {
                 sb.append(c);
             }
         }
-        //去除多余的左括号(从前往后删除也行)
-        for (int i = sb.length() - 1; i >= 0 && leftCount > 0; i--) {
+         for (int i = sb.length() - 1; i >= 0 && count > 0; i--) {
             if (sb.charAt(i) == '('){
                 sb.deleteCharAt(i);
-                leftCount--;
+                count--;
             }
         }
         return sb.toString();
