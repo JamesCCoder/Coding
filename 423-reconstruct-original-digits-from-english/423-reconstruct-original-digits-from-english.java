759c749013c09b9211f868158d5e7ff19dac1651
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-12 11:02:41 -0600

    Time: 8 ms (70.55%), Space: 42.7 MB (66.86%) - LeetHub

--- 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.java
+++ 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.java
@@ -1,30 +1,31 @@
 class Solution {
-public String originalDigits(String s) {
-    int[] count = new int[10];
-    for (int i = 0; i < s.length(); i++){
-        char c = s.charAt(i);
-        if (c == 'z') count[0]++;
-        if (c == 'w') count[2]++;
-        if (c == 'x') count[6]++;
-        if (c == 's') count[7]++; //7-6
-        if (c == 'g') count[8]++;
-        if (c == 'u') count[4]++; 
-        if (c == 'f') count[5]++; //5-4
-        if (c == 'h') count[3]++; //3-8
-        if (c == 'i') count[9]++; //9-8-5-6
-        if (c == 'o') count[1]++; //1-0-2-4
-    }
-    count[7] -= count[6];
-    count[5] -= count[4];
-    count[3] -= count[8];
-    count[9] = count[9] - count[8] - count[5] - count[6];
-    count[1] = count[1] - count[0] - count[2] - count[4];
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i <= 9; i++){
-        for (int j = 0; j < count[i]; j++){
-            sb.append(i);
+    public String originalDigits(String s) {
+        int[] in = new int[10];
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == 'z') in[0]++;
+            if(c == 'w') in[2]++;
+            if(c == 'u') in[4]++;
+            if(c == 'x') in[6]++;
+            if(c == 'g') in[8]++;               //12333
+            if(c == 's') in[7]++;
+            if(c == 'f') in[5]++;
+            if(c == 'h') in[3]++;
+            if(c == 'i') in[9]++;
+            if(c == 'o') in[1]++;               //11221
         }
+        in[7] -= in[6];                            //76
+        in[5] -= in[4];                            //54
+        in[3] -= in[8];                            //38
+        in[9] = in[9] - in[8] - in[5] - in[6];     //9856
+        in[1] = in[1] - in[0] - in[2] - in[4];     //1024\
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<= 9; i++){
+            for(int j = 0; j<in[i]; j++){
+                sb.append(i);
+            }
+        }
+        return sb.toString();
     }
-    return sb.toString();
-}
 }
