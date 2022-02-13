594f0afbf8fc137959191bf2cfa6d31c369d6340
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-13 10:45:05 -0600

    Time: 0 ms (100.00%), Space: 67.5 MB (8.15%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -14,21 +14,25 @@
  * }
  */
 class Solution {
-    int ans;
-    public int rangeSumBST(TreeNode root, int L, int R) {
-        ans = 0;
-        dfs(root, L, R);
-        return ans;
+    int res;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        res = 0;
+        helper(root, low, high);
+        return res;
     }
-
-    public void dfs(TreeNode node, int L, int R) {
-        if (node != null) {
-            if (L <= node.val && node.val <= R)
-                ans += node.val;
-            if (L < node.val)
-                dfs(node.left, L, R);
-            if (node.val < R)
-                dfs(node.right, L, R);
+    
+    public void helper(TreeNode node, int low, int high){
+        if(node != null){
+            if(low <= node.val && node.val <= high){
+                res += node.val;
+               
+            }
+             if(low < node.val){
+                    helper(node.left, low, high);
+                }
+                if(node.val < high){
+                    helper(node.right, low, high);
+                }
         }
     }
 }
