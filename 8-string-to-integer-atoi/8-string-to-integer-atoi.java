e127faa6615839fa4bff6c8841ba59d747fa0664
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-14 10:39:05 -0600

    Time: 3 ms (66.29%), Space: 42.9 MB (12.15%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,32 +1,33 @@
 class Solution {
-    public int myAtoi(String str) {
-        str = str.trim();
-        if(str == null || str.length() == 0) return 0;
-        
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s == null || s.length() == 0){
+            return 0;
+        }
         int sign = 1;
         int idx = 0;
-        char c = str.charAt(0);
-        if(c == '+'){
+        if(s.charAt(0) == '+'){
             sign = 1;
             idx++;
-        }else if(c == '-'){
+        }
+        if(s.charAt(0) == '-'){
             sign = -1;
             idx++;
         }
         
         long sum = 0;
-        for(int i = idx; i<str.length(); i++){
-            if(!Character.isDigit(str.charAt(i))){
+        for(int i = idx; i<s.length(); i++){
+            if(!Character.isDigit(s.charAt(i))){
                 return (int)sum * sign;
             }
-            sum = sum * 10 + str.charAt(i) -'0';
-            if(sign == 1 && sum>Integer.MAX_VALUE){
+            sum = sum * 10 + s.charAt(i) - '0';
+            if(sign == 1 && sum > Integer.MAX_VALUE){
                 return Integer.MAX_VALUE;
             }
-            if(sign == -1 && (-1) * sum <Integer.MIN_VALUE){
+            if(sign == -1 && (-1) * sum < Integer.MIN_VALUE){
                 return Integer.MIN_VALUE;
             }
         }
-        return (int)sum * sign;
+        return (int)sum*sign;
     }
 }
