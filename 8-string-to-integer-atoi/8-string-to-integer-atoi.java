644c74a628e7b3d15fe518304fdca91d57a8aa8a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-14 13:30:58 -0600

    Time: 2 ms (83.16%), Space: 42.8 MB (12.15%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,33 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
         s = s.trim();
-        if(s == null || s.length() == 0){
-            return 0;
-        }
+        if(s == null || s.length() == 0) return 0;
+        
         int sign = 1;
-        int idx = 0;
+        int index = 0;
         if(s.charAt(0) == '+'){
             sign = 1;
-            idx++;
+            index++;
         }
         if(s.charAt(0) == '-'){
             sign = -1;
-            idx++;
+            index++;
         }
         
-        long sum = 0;
-        for(int i = idx; i<s.length(); i++){
+        long res = 0;
+        for(int i = index; i<s.length(); i++){
             if(!Character.isDigit(s.charAt(i))){
-                return (int)sum * sign;
+                return (int)res * sign;
             }
-            sum = sum * 10 + s.charAt(i) - '0';
-            if(sign == 1 && sum > Integer.MAX_VALUE){
+            res = res * 10 + s.charAt(i) - '0';
+            if(sign == 1 && res > Integer.MAX_VALUE){
                 return Integer.MAX_VALUE;
             }
-            if(sign == -1 && (-1) * sum < Integer.MIN_VALUE){
+            if(sign == -1 && sign * res < Integer.MIN_VALUE){
                 return Integer.MIN_VALUE;
             }
         }
-        return (int)sum*sign;
+        return (int)res*sign;
     }
 }
