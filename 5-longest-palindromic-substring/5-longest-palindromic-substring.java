b02fabf8320147c3d68b8a4de21824e4a62342fd
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-14 09:47:19 -0600

    Time: 364 ms (26.11%), Space: 94.8 MB (18.95%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -3,13 +3,13 @@ class Solution {
         boolean[][] dp = new boolean[s.length()][s.length()];
         int max = 0;
         String res = "";
-        for(int j = 0; j<s.length(); j++){
-            for(int i = 0; i<= j; i++){
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i+1][j-1]);
+        for(int j = 0; j< s.length(); j++){
+            for(int i = 0; i<=j; i++){
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i <=2 || dp[i+1][j-1]);
                 if(dp[i][j]){
                     if(j - i + 1 > max){
                         max = j - i + 1;
-                        res = s.substring(i, j+1);
+                        res = s.substring(i, j + 1);
                     }
                 }
             }
