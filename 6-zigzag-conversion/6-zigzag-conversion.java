962f0e462eb542f0bcba5b7226c021c4251a22b2
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-14 09:54:43 -0600

    Time: 11 ms (61.83%), Space: 48.8 MB (33.71%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,31 +1,26 @@
 class Solution {
     public String convert(String s, int numRows) {
-        int n = s.length();
-        if(numRows > n || numRows == 1) return s;
-        
-        List<StringBuilder> rows = new ArrayList<>();
-        
+        if(numRows == 1 || s.length() < numRows) return s;
+        List<StringBuilder> list = new ArrayList<>();
         for(int i = 0; i<numRows; i++){
-            rows.add(new StringBuilder());
+            list.add(new StringBuilder());
         }
         
         int curRow = 0;
-        boolean canGoDown = false;
-        for(char c : s.toCharArray()){
+        boolean canChange = false;
+        
+        for(int i = 0; i<s.length(); i++){
             if(curRow == 0 || curRow == numRows - 1){
-                canGoDown = !canGoDown;
+                canChange = !canChange;
             }
-            
-            rows.get(curRow).append(c);
-            
-            curRow += canGoDown ? 1 : -1;
+            list.get(curRow).append(s.charAt(i));
+            curRow += canChange ? 1 : -1;
         }
         
         StringBuilder res = new StringBuilder();
-        for(StringBuilder sb: rows){
+        for(StringBuilder sb: list){
             res.append(sb);
         }
-        
         return res.toString();
     }
 }
