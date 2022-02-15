d0e61e3eb8d2e6ac84a6922cf248d25dd9788b3f
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-15 17:55:00 -0600

    Time: 6 ms (54.74%), Space: 48.5 MB (5.96%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,41 +1,36 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int N = 9;
-        
         HashSet<Character>[] row = new HashSet[N];
         HashSet<Character>[] col = new HashSet[N];
         HashSet<Character>[] table = new HashSet[N];
         
-        
         for(int i = 0; i<N; i++){
-            row[i] = new HashSet<Character>();
-            col[i] = new HashSet<Character>();
-            table[i] = new HashSet<Character>();
+            row[i] = new HashSet<>();
+            col[i] = new HashSet<>();
+            table[i] = new HashSet<>();
         }
         
         for(int i = 0; i<N; i++){
             for(int j = 0; j<N; j++){
-                char val = board[i][j];
+                char c = board[i][j];
+                if(c =='.') continue;
                 
-                if(val == '.'){
-                    continue;
-                }
-                
-                if(row[i].contains(val)){
+                if(row[i].contains(c)){
                     return false;
                 }
-                row[i].add(val);
+                row[i].add(c);
                 
-                if(col[j].contains(val)){
+                if(col[j].contains(c)){
                     return false;
                 }
-                col[j].add(val);
+                col[j].add(c);
                 
-                int index = (i/3)*3 + j/3;
-                if(table[index].contains(val)){
+                int k = (i/3)*3 + j/3;
+                if(table[k].contains(c)){
                     return false;
                 }
-                table[index].add(val);
+                table[k].add(c);
             }
         }
         return true;
