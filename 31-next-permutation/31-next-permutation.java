6ed4c186d8fe207cc4dbc36a360a4193e229c8a2
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-15 16:50:45 -0600

    Time: 3 ms (7.81%), Space: 43.8 MB (15.43%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,19 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        for(int i = nums.length - 1; i>0; i--){
-             if(nums[i] - nums[i-1] > 0){
-                 Arrays.sort(nums, i, n);
-             }
-             for(int j = i; j< nums.length; j++){
-                 if(nums[j] - nums[i-1] > 0){
-                     int tmp = nums[j];
-                     nums[j] = nums[i-1];
-                     nums[i-1] = tmp;
-                     return;
-                 }
-             }
-         }
+        for(int i = nums.length - 1; i> 0; i--){
+            if(nums[i] - nums[i-1] > 0){
+                Arrays.sort(nums, i, n);
+            }
+            for(int j = i; j<n; j++){
+                if(nums[j] - nums[i-1] > 0){
+                    int tmp = nums[j];
+                    nums[j] = nums[i-1];
+                    nums[i-1] = tmp;
+                    return;
+                }
+            }
+        }
         Arrays.sort(nums);
         return;
     }
