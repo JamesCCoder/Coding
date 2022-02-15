c2225a122730fbe3b0428e6479222be76976bd93
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-15 16:36:49 -0600

    Time: 240 ms (11.25%), Space: 247.9 MB (5.15%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -12,17 +12,16 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length == 0) return null;
         if(lists.length == 1) return lists[0];
-        
-        ListNode res = mergeNode(lists[0], lists[1]);
+        ListNode res = helper(lists[0], lists[1]);
         for(int i = 2; i<lists.length; i++){
-            res = mergeNode(res, lists[i]);
+            res = helper(res, lists[i]);
         }
         return res;
     }
     
-    public ListNode mergeNode(ListNode l1, ListNode l2){
-        ListNode list = new ListNode(0);
-        ListNode cur = list;
+    public ListNode helper(ListNode l1, ListNode l2){
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
         while(l1 != null && l2 != null){
             if(l1.val < l2.val){
                 cur.next = new ListNode(l1.val);
@@ -31,7 +30,7 @@ class Solution {
                 cur.next = new ListNode(l2.val);
                 l2 = l2.next;
             }
-            cur =cur.next;
+            cur = cur.next;
         }
         if(l1 == null){
             cur.next = l2;
@@ -39,6 +38,6 @@ class Solution {
         if(l2 == null){
             cur.next = l1;
         }
-        return list.next;
+        return res.next;
     }
 }
