f2e6f76a85c3e2bb380590c719e8223622ee2455
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-15 14:44:27 -0600

    Time: 24 ms (78.46%), Space: 58.9 MB (31.84%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,26 +3,26 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         
-
         for(int i = 0; i<nums.length; i++){
             if(nums[i] > 0) break;
             if(i > 0 && nums[i] == nums[i-1]) continue;
             
             int l = i + 1;
-            int r = nums.length -1;
+            int r = nums.length - 1;
             int target = 0 - nums[i];
             
             while(l < r){
                 int tmp = nums[l] + nums[r];
                 if(tmp == target){
-                    res.add(Arrays.asList(new Integer[]{nums[i], nums[l], nums[r]}));
+                    res.add(Arrays.asList(new Integer[]{nums[l], nums[r], nums[i]}));
                     while(l < r && nums[l] == nums[l-1]){
                         l++;
                     }
                     r--;
                     while(l < r && nums[r] == nums[r+1]){
-                        r--;
+                         r--;
                     }
+                   
                 }else if(tmp < target){
                     l++;
                 }else{
