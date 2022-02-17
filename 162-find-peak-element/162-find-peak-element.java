e89054addca156a65c78039f3b0b6bfdbca854b1
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-16 22:31:49 -0600

    Time: 0 ms (100.00%), Space: 41.5 MB (33.02%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        for(int i = 0; i<nums.length-1; i++){
-            if(nums[i]>nums[i+1]){
+        for(int i = 0; i<nums.length - 1; i++){
+            if(nums[i] - nums[i+1] > 0){
                 return i;
             }
         }
