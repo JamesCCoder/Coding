054ab2bebae9734b58f752a4443aaefc1712d47b
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-17 22:03:54 -0600

    Time: 16 ms (84.79%), Space: 50.1 MB (46.02%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -2,22 +2,22 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         StringBuilder sb = new StringBuilder();
         int count = 0;
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i<s.length(); i++){
             char c = s.charAt(i);
-            if (c == '('){
+            if(c == '('){
                 count++;
                 sb.append(c);
-            } else if (c == ')'){
-                if (count > 0){
+            }else if(c == ')'){
+                if(count > 0){
                     count--;
                     sb.append(c);
                 }
-            } else {
+            }else{
                 sb.append(c);
             }
         }
-         for (int i = sb.length() - 1; i >= 0 && count > 0; i--) {
-            if (sb.charAt(i) == '('){
+        for(int i = sb.length() - 1; i>=0 && count > 0; i--){
+            if(sb.charAt(i) == '('){
                 sb.deleteCharAt(i);
                 count--;
             }
@@ -25,4 +25,3 @@ class Solution {
         return sb.toString();
     }
 }
-
