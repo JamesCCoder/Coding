3dfd077a17e87ed5704e7b704778c17484a1be0a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-17 23:50:33 -0600

    Time: 1 ms (53.06%), Space: 41 MB (16.30%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,25 +1,15 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int sum = 0;
+        int res = 0;
         while(num != 0){
             if(num % 2 == 0){
-            num = even(num);
-            sum ++;
-        }else{
-            num = odd(num);
-            sum ++;
+                num /= 2;
+                res++;
+            }else{
+                num -= 1;
+                res++;
+            }
         }
-        }
-        return sum;
-    }
-    
-    public int odd(int n){
-        n = n -1;
-        return n;
-    }
-    
-    public int even(int m){
-        m = m/2;
-        return m;
+        return res;
     }
 }
