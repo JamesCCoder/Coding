e400ab79116c751dfafb5f3cda6904e20434ce8d
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-18 00:31:08 -0600

    Time: 2 ms (63.77%), Space: 42.8 MB (10.14%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,17 +1,14 @@
 class Solution {
     public int maxPower(String s) {
         int n = 1;
-        int res = Integer.MIN_VALUE;
+        int res = 1;
         for(int i = 0; i<s.length()-1; i++){
             if(s.charAt(i) == s.charAt(i+1)){
                 n++;
-                if(n>res){res = n;}
+                res = Math.max(res, n);
             }else{
                 n = 1;
             }
-        }
-        if(res<0){
-            res = 1;
         }
         return res;
     }
