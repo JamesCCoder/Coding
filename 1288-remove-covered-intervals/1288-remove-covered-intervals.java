fd59b1192f9ee708a67cdee56deb29ff7c90b9e8
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-19 21:41:14 -0600

    Time: 6 ms (71.14%), Space: 47.3 MB (5.77%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,13 +1,13 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-     Arrays.sort(intervals, (a, b) -> a[0]==b[0]?Integer.compare(b[1],a[1]):Integer.compare(a[0], b[0]));
-        int count = 1;
-        for (int i = 0, j = 1; j < intervals.length; j++) {
-            if (intervals[i][1] < intervals[j][1]) {
+        int res = 1;
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0]- b[0]);
+        for(int i = 0, j = 1; j<intervals.length; j++){
+            if(intervals[i][1] < intervals[j][1]){
                 i = j;
-                count++;
+                res++;
             }
         }
-        return count;
+        return res;
     }
 }
