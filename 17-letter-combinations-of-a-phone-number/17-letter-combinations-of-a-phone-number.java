e408eedfcff6a3ccc7623d753caf1bd9ea9ef844
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-21 14:48:15 -0600

    Time: 1 ms (89.34%), Space: 42.9 MB (14.70%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,26 @@
 class Solution {
-    String[] keyboard = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    String[] board = new String[]{"", "", "abc", "def", "ghi", "jkl","mno", "pqrs", "tuv","wxyz"};
     List<String> res = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
         if(digits.length() == 0) return res;
-        helper(digits, new StringBuffer(), 0);
+        helper(digits, new StringBuilder(), 0);
         return res;
-        
     }
     
-    public void helper(String digits, StringBuffer cur, int index){
+    public void helper(String digits, StringBuilder cur, int index){
         if(index == digits.length()){
             res.add(cur.toString());
             return;
         }
-        String letters = keyboard[digits.charAt(index)-'0'];
-        for(int i = 0;i<letters.length(); i++){
-            char c = letters.charAt(i);
+        
+        String letter = board[digits.charAt(index) - '0'];
+        for(int i = 0; i< letter.length(); i++){
+            char c = letter.charAt(i);
             cur.append(c);
             helper(digits, cur, index + 1);
             cur.deleteCharAt(cur.length()-1);
         }
         
-    } 
+    }
 }
