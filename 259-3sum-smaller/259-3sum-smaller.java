53eb0ccd01cd8b242ed03849ec757b3de096aa58
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-22 21:29:59 -0600

    Time: 11 ms (76.82%), Space: 43.7 MB (9.36%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.java
+++ 259-3sum-smaller/259-3sum-smaller.java
@@ -3,11 +3,11 @@ class Solution {
         int res = 0;
         Arrays.sort(nums);
         
-        for(int i = 0; i<nums.length - 2; i++){
-            int l = i + 1;
+        for(int i = 0; i<nums.length -2; i++){
+            int l = i+1;
             int r = nums.length - 1;
             while(l < r){
-                int sum = nums[i] + nums[l] + nums[r];
+                int sum= nums[l] + nums[r] + nums[i];
                 if(sum < target){
                     res += r - l;
                     l++;
