3fa024b22038b166644954585e5fff3176fb64e5
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-22 21:15:46 -0600

    Time: 22 ms (95.16%), Space: 42.1 MB (40.16%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -18,24 +18,22 @@ class Node {
 }
 */
 
-
 class Solution {
-    public HashMap<Integer, Node> map = new HashMap<>();
-    
+    HashMap<Integer, Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
         return clone(node);
     }
     
-    public Node clone(Node node) {
-        if (node == null) return null;
-        
-        if (map.containsKey(node.val)) 
+    public Node clone(Node node){
+        if(node == null) return null;
+        if(map.containsKey(node.val)){
             return map.get(node.val);
-        
+        }
         Node newNode = new Node(node.val, new ArrayList<Node>());
         map.put(newNode.val, newNode);
-        for (Node neighbor : node.neighbors) 
+        for(Node neighbor: node.neighbors){
             newNode.neighbors.add(clone(neighbor));
+        }
         return newNode;
     }
 }
