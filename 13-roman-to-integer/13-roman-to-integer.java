1897a427b765309924f2b3bf5f6d47cbede89fbf
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-23 20:38:59 -0600

    Time: 28 ms (6.54%), Space: 49.8 MB (5.06%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -18,19 +18,19 @@ class Solution {
         int res = 0;
         int idx = 0;
         while(idx < s.length()){
-            if(idx < s.length()- 1){
+            if(idx < s.length() - 1){
                 String db = s.substring(idx, idx + 2);
                 if(map.containsKey(db)){
-                    res = res + map.get(db);
-                    idx = idx + 2;
+                    res += map.get(db);
+                    idx += 2;
                     continue;
                 }
-                
             }
-            String sg = s.substring(idx, idx + 1);
-            if(map.containsKey(sg)){
-                res = res + map.get(sg);
-                idx = idx + 1;
+                String sg = s.substring(idx, idx + 1);
+                if(map.containsKey(sg)){
+                    res += map.get(sg);
+                    idx += 1;
+                
             }
         }
         return res;
