18fe19d898e37707ab069d86be550a77fefd8946
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-23 22:41:51 -0600

    Time: 5 ms (64.53%), Space: 44.8 MB (36.83%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,22 +1,21 @@
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        helper(res, new ArrayList<>(), candidates, target, 0);
+        helper(new ArrayList<>(), candidates, target, 0);
         return res;
     }
     
-    public void helper(List<List<Integer>> res, List<Integer> tmp, int[] candidates, int target, int index){
+    public void helper(List<Integer> cur, int[] candidates, int target, int index){
         if(target <= 0){
             if(target == 0){
-                res.add(new ArrayList<>(tmp));
+                res.add(new ArrayList(cur));
             }
             return;
         }
-        
         for(int i = index; i<candidates.length; i++){
-            tmp.add(candidates[i]);
-            helper(res, tmp, candidates, target-candidates[i], i);
-            tmp.remove(tmp.size()-1);
+            cur.add(candidates[i]);
+            helper(cur, candidates, target - candidates[i], i);
+            cur.remove(cur.size()-1);
         }
     }
 }
