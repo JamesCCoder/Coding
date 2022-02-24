c1d58ec6217bd4f8241e567349b07cbba4ab63f0
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-24 17:12:48 -0600

    Time: 0 ms (100.00%), Space: 42.7 MB (10.03%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,10 +2,11 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
+        
         int[][] res = new int[m][n];
         res[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
         
-        for(int i = 0; i< m; i++){
+        for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(obstacleGrid[i][j] != 1){
                     if(i > 0 && j > 0){
@@ -15,10 +16,9 @@ class Solution {
                     }else if(j > 0){
                         res[i][j] = res[i][j-1];
                     }
-                    
                 }
             }
         }
-        return res[m-1][n-1];
+            return res[m - 1][n - 1];
     }
 }
