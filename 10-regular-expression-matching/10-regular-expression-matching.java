a6128964d47b4563c42b0da50760309011068494
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-23 20:03:30 -0600

    Time: 154 ms (12.23%), Space: 118.3 MB (5.42%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         if(p.length() == 0) return s.length() == 0;
-        boolean firstMatch = s.length()>0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        boolean first = s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
         if(p.length() >= 2 && p.charAt(1) == '*'){
-            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
+            return isMatch(s, p.substring(2)) || (first && isMatch(s.substring(1), p));
         }else{
-            return firstMatch && isMatch(s.substring(1), p.substring(1));
+            return first && isMatch(s.substring(1), p.substring(1));
         }
     }
 }
