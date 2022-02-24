d9391b625e173706a080f5ae521d1f288c88711b
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-24 11:38:16 -0600

    Time: 3 ms (80.95%), Space: 48.4 MB (25.33%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,13 +1,14 @@
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        
         if(nums.length < 1) return res;
         Arrays.sort(nums);
-        helper(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        helper(new ArrayList<>(), nums, new boolean[nums.length]);
         return res;
     }
     
-    public void helper(List<List<Integer>> res, List<Integer> cur, int[] nums, boolean[] visited){
+    public void helper(List<Integer> cur, int[] nums, boolean[] visited){
         if(cur.size() == nums.length){
             res.add(new ArrayList(cur));
             return;
@@ -18,7 +19,7 @@ class Solution {
             if(i>0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
             cur.add(nums[i]);
             visited[i] = true;
-            helper(res, cur, nums, visited);
+            helper(cur, nums, visited);
             cur.remove(cur.size()-1);
             visited[i] = false;
         }
