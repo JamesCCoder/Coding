83d13b48402a36d4e378564fa4abcf181a5ae661
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-25 23:07:38 -0600

    Time: 8 ms (65.96%), Space: 45.1 MB (18.24%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -2,15 +2,16 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        int[][] dp = new int[m+1][n+1];
         
-        for(int i = 0; i<=m; i++){
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-            dp[i][0] = i;
+        int[][] res = new int[m+1][n+1];
+        
+        for(int i = 0; i<= m; i++){
+            Arrays.fill(res[i], Integer.MAX_VALUE);
+            res[i][0] = i;
         }
         
         for(int j = 0; j<=n; j++){
-            dp[0][j] = j;
+            res[0][j] = j;
         }
         
         for(int i = 0; i<m; i++){
@@ -19,12 +20,12 @@ class Solution {
                 char c2 = word2.charAt(j);
                 
                 if(c1 == c2){
-                    dp[i+1][j+1] = dp[i][j];
+                    res[i+1][j+1] = res[i][j];
                 }else{
-                    dp[i+1][j+1] = Math.min(Math.min(dp[i][j+1], dp[i+1][j]), dp[i][j]) +1;
+                    res[i+1][j+1] = Math.min(Math.min(res[i+1][j], res[i][j+1]), res[i][j]) + 1;
                 }
             }
         }
-        return dp[m][n];
+        return res[m][n];
     }
 }
