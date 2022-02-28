afe8eb9a38b123f7d01f0c2b5a8de5624d9fe62f
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-02-27 18:33:13 -0600

    Time: 13 ms (61.56%), Space: 55 MB (31.98%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
         int[][] res = new int[intervals.length][2];
+        Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
         int idx = 0;
         for(int i = 0; i<intervals.length; i++){
-            if(i == 0 || intervals[i][0] > res[idx-1][1]){
+            if(idx == 0 || intervals[i][0] > res[idx-1][1]){
                 res[idx] = intervals[i];
                 idx++;
             }else{
-                res[idx - 1][1] = Math.max(res[idx- 1][1], intervals[i][1]);
+                res[idx-1][1] = Math.max(res[idx-1][1], intervals[i][1]);
             }
         }
         return Arrays.copyOf(res, idx);
