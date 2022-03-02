492a596e0ff65e6f125c63866b3be09a8b7303b2
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-03-01 19:41:45 -0600

    Time: 6 ms (62.57%), Space: 43.3 MB (53.20%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        for(int i = 0, j = s.length() - 1; i < j; i++, j--){
+        for(int i = 0, j = s.length()-1; i < j; i++, j--){
             while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
             }
