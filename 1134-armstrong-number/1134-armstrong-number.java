35aebe2fc9d31abae04acb54624faa76b6ed674a
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-03-05 16:53:31 -0600

    Time: 0 ms (100.00%), Space: 41.1 MB (23.00%) - LeetHub

--- 1134-armstrong-number/1134-armstrong-number.java
+++ 1134-armstrong-number/1134-armstrong-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isArmstrong(int n) {
-        int m = Integer.toString(n).length();
-        int res = 0;
+        String s = Integer.toString(n);
+        int m = s.length();
+        
         int tmp = n;
+        int res = 0;
+        
         while(tmp != 0){
-            res += Math.pow(tmp % 10,m);
+            res += Math.pow(tmp % 10, m);
             tmp /= 10;
         }
+        
         return res == n;
     }
 }
