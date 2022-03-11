353fd4881b9151a5b83442240c4f2983a415043c
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-03-11 15:00:47 -0600

    Time: 1 ms (28.96%), Space: 42.7 MB (25.47%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,12 +1,10 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        Arrays.sort(nums);
         int i = 0;
         for(int j = 0; j<nums.length; j++){
             if(nums[j] != val){
                 nums[i] = nums[j];
                 i++;
-                
             }
         }
         return i;
