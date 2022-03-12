be6deb2d9a33da7891ea1de208a8a8eb987d6ced
James W <95759159+JamesCCoder@users.noreply.github.com>
2022-03-11 20:21:54 -0600

    Time: 2 ms (85.49%), Space: 81.3 MB (9.07%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -4,7 +4,7 @@ class Solution {
         int cur = 0;
         int res = 0;
         for(int i = 0; i<gas.length; i++){
-            total += gas[i]- cost[i];
+            total += gas[i] - cost[i];
             cur += gas[i] - cost[i];
             if(cur < 0){
                 cur = 0;
